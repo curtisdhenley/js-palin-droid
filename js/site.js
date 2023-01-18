f30d8a5081e0bae1e28dc26dbd06d530f7c1b5f9
Curtis Henley <curtisdhenley@gmail.com>
2023-01-18 17:03:58 -0500

    completed challenge

--- js/site.js
+++ js/site.js
@@ -1,11 +1,42 @@
-function displayMsg() {
-    let msg = document.getElementById('message').value;
+// entry point AKA Controller
+function getValues() {
+    let userInput = document.getElementById('userInput').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    let palindromeString = checkForPalindrome(userInput);
+
+    displayResults(palindromeString);
+}
+
+// logic function
+// check to see if the input is a palindrome
+function checkForPalindrome(userInput) {
+    let regex = /[\W_]/g;
+    let trimmedUserInput = userInput.toLowerCase().replace(regex, '');
+    let palindromeString = ''
+
+    for (let i = (trimmedUserInput.length - 1); i >= 0; i--) {
+        palindromeString += trimmedUserInput[i];
+    }
+
+    if (palindromeString != trimmedUserInput) {
+        // if it's not a palindrome, tell our user!
+        Swal.fire(
+            {
+                icon: 'error',
+                title: 'Oops!',
+                text: `"${userInput}" is not a Palindrome!`
+            }
+        ); 
+    } else {
+        return palindromeString;
+    }
+
+}
+
+// view function
+function displayResults(palindromeString) {
+    if (palindromeString) {    
+        document.getElementById('results').innerHTML = `Your word or sentence reversed is: ${palindromeString}`;
+        document.getElementById('alert').classList.remove('invisible')
+    }
 }
